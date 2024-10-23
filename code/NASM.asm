17f399908f312a93f49555a4c94d48a472ffef64
Ryedis <134209766+Ryedis@users.noreply.github.com>
2024-10-23 23:27:22 +0400

    add assembler code

--- code/NASM.asm
+++ code/NASM.asm
@@ -1,37 +1,47 @@
 section .data
-    prompt_size db "Введите размер массива: ", 0
-    prompt_elements db "Введите элементы массива:", 0
-    result_msg db "Количество уникальных элементов: ", 0
-    arr resw 100
+    prompt_size db "Enter array size: ", 0
+    error_size db "Error: invalid array size.", 10, 0
+    prompt_elements db "Enter array elements:", 0
+    error_element db "Error: invalid array element.", 10, 0
+    result_msg db "Number of unique elements: %d", 10, 0
+    format_int db "%d", 0
+    format_short db "%hd", 0
+    format_char db "%c", 0
 
 section .bss
+    arr resw 100
+    arr_size resd 1
     unique_count resd 1
     i resd 1
     j resd 1
     is_unique resb 1
+    input resb 20
 
 section .text
-    extern printf, scanf
+    extern printf, scanf, getchar
     global main
 
 main:
-    ; Выводим сообщение для ввода размера массива
+    ; Enter array size
     push prompt_size
     call printf
     add esp, 4
-
-    ; Вводим размер массива
+    
     push arr_size
     push format_int
     call scanf
     add esp, 8
 
-    ; Выводим сообщение для ввода элементов массива
+    cmp dword [arr_size], 0
+    jle error_size_input
+    cmp dword [arr_size], 100
+    jg error_size_input
+
+    ; Enter array elements
     push prompt_elements
     call printf
     add esp, 4
 
-    ; Вводим элементы массива
     mov eax, 0
     mov [i], eax
 
@@ -40,16 +50,36 @@ input_loop:
     cmp eax, [arr_size]
     jge end_input
 
-    push arr + eax * 2
+    mov ebx, arr
+    mov edx, eax
+    shl edx, 1
+    add ebx, edx
+    push ebx
     push format_short
     call scanf
     add esp, 8
 
+    ; Check correct input
+    cmp eax, 1
+    jne error_element_input
+
     inc dword [i]
     jmp input_loop
 
+error_size_input:
+    push error_size
+    call printf
+    add esp, 4
+    jmp end_program
+
+error_element_input:
+    push error_element
+    call printf
+    add esp, 4
+    jmp end_program
+
 end_input:
-    ; Считаем уникальные элементы
+    ; Counting unique elements
     mov eax, 0
     mov [i], eax
     mov [unique_count], eax
@@ -68,9 +98,10 @@ compare_loop:
     cmp eax, [i]
     jge end_compare
 
-    ; Сравниваем элементы arr[i] и arr[j]
+    ; Comparing the elements arr[i] and arr[j]
     movzx eax, word [arr + eax * 2]
-    movzx ebx, word [arr + [i] * 2]
+    mov ebx, [i]
+    movzx ebx, word [arr + ebx * 2]
     cmp eax, ebx
     jne not_equal
 
@@ -92,11 +123,11 @@ not_unique:
     jmp count_unique_loop
 
 end_count:
-    ; Выводим результат
-    push dword [unique_count]
+    mov eax, [unique_count]
+    push eax
     push result_msg
     call printf
     add esp, 8
 
-    ; Завершаем программу
+end_program:
     ret
